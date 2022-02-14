dcfe9c9dd5f33737c7f4cdbcbdf437c3868f7515
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-14 17:41:36 +0100

    hleop

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/14 17:38:59 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/02/14 17:41:13 by xcarroll         ###   ########.fr       */
+/*   Created: 2022/02/14 17:38:54 by xcarroll          #+#    #+#             */
+/*   Updated: 2022/02/14 17:41:10 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putchar(char c);
 
-void	ft_putchar(char c)
+void	ft_print_alphabet(void)
 {
-	write(1, &c, 1);
+	char	alphabet;
+
+	write(1, &alphabet, 1);
 }
