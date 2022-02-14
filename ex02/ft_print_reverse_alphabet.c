31fe210e0682ef6cd4c6aa412ebcda8e7d6b0507
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-14 18:28:59 +0100

    helpppp

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/14 17:38:54 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/02/14 18:25:41 by xcarroll         ###   ########.fr       */
+/*   Created: 2022/02/14 18:28:42 by xcarroll          #+#    #+#             */
+/*   Updated: 2022/02/14 18:28:48 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,10 @@ void	ft_print_alphabet(void)
 {
 	char	alphabet;
 
-	alphabet = 'a';
-	while (alphabet <= 'z')
+	alphabet = 'z';
+	while (alphabet >= 'a')
 	{
 		write(1, &alphabet, 1);
-		alphabet++;
+		alphabet--;
 	}
-}
-
-int	main(void)
-{
-	ft_print_alphabet();
 }
