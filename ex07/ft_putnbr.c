a77c17e9f3e968ad4bf22286af5794f23ce85751
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-16 14:01:17 +0100

    ex07

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 21:53:35 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/02/15 22:58:38 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/02/16 14:01:07 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,38 @@
 
 void	ft_putchar(char c)
 {
+	/* printf("%d", c);*/
 	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	int	i;
+	int		first_digit;
+	char	chars[10];
 
-	i = 0;
-	while (i >= 10)
+	chars = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	if (nb < 0)
 	{
-		i = i / 10;
+		ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			ft_putchar('2');
+			nb = 147483648;
+		}
+		else
+		{
+			nb *= -1;
+		}
 	}
+	first_digit = nb;
+	while (first_digit >= 10)
+	{
+		first_digit = first_digit / 10;
+	}
+	ft_putchar(first_digit + '0');
 }
 
 int	main(void)
 {
-	ft_putnbr(42);
+	ft_putnbr(-42069);
 }
